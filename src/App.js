82fc5f24125d66dfc980f18cd99f4db7759f8c4f
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2024-02-26 17:05:43 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NavBar from './components/NavBar';
+import Accountuser from './pages/Accountuser';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/accountuser' element={<Accountuser />} />
         </Routes>
       </main>
       <Footer />
